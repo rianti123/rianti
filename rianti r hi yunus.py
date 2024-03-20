7d2afba934d2aa0c35f5c8d270b819c1ff013241
rianti123 <164118383+rianti123@users.noreply.github.com>
2024-03-19 20:01:58 -0700

    Add files via upload

--- rianti r hi yunus.py
+++ rianti r hi yunus.py
@@ -1,31 +1,7 @@
-def hitung_gaji_harian(gaji_pokok):
-    return gaji_pokok / 8
-
-def hitung gaji_mingguan(gaji_pokok):
-    eturn gaji_pokok / 48
-
-def main():
-    nama_karyawan input("Nama karyawan: ")
-    gaji_pokok float(input("Masukkan gaji pokok: "))
-    jam_kerja_per_hari = 8
-
-    print("\nNama karyawan:", nama_karyawan)
-    print("Gaji pokok:", gaji_pokok)
-    print("Jumlah jam kerja per hari:", jam_kerja_per_hari,"Jam")
-
-    print("\nPilih Rincian Gaji Mingguan/Harian:")
-    print("1. Gaji Harian")
-    print("2. Gaji Mingguan")
-    pilihan = int(input("Pilih [1/2]: "))
-
-    if pilihan == 1:
-        gaji_harian = hitung_gaji_harian(gaji_pokok)
-        print("Gaji harian:", gaji harian)
-    elif pilihan == 2
-        gaji_mingguan = hitung_gaji_mingguan(gaji_pokok)
-        print("Gaji mingguan:", gaji_mingguan)
-    else:
-        ("prin pilihan tidak falid")
-
- if_name__ == "main_":
-    main()
+bilangan=int(input("masukan bilangan yang ingin dikonversi :\t"))
+konversi1=bin(bilangan)[2:]
+konversi2=hex(bilangan)
+konversi3=oct(bilangan)
+print("Bilangan Biner \t\t: ", konversi1.zfill(8))
+print("Bilangan Hexadecimal \t: ", konversi2)
+print("Bilangan Octal \t\t: ", konversi3)
